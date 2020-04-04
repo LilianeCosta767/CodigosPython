97545d422f9a6cf5a063a9792430b93b95f11a75
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-04-04 19:26:31 -0300

    codigo correto do multiplos de cinco - questao thehuxley

--- MultiplosdeCinco.py
+++ MultiplosdeCinco.py
@@ -4,9 +4,10 @@ n = int(valores[0]) #inicio
 m = int(valores[1]) #fim
 while(n <= m):
     if(n % 5 == 0):
-        if(n < m):
+        if(m - n < 5):
+            print('{}'.format(n), end="")
+        elif(n < m):
             print('{}|'.format(n), end="")
         else:
-            print('{}'.format(n), end="")
+            print('{}'.format(n))
     n = n + 1
-
