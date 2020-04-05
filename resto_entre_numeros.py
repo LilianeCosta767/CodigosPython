9b3adc0749ab76b9c6d4d0b8096a9a65e5bf5a87
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-04-05 10:14:39 -0300

    resto entre numeros - questao thehuxley com for

--- resto_entre_numeros.py
+++ resto_entre_numeros.py
@@ -1,4 +1,13 @@
-X = input()
-Y = input()
+X = int(input())
+Y = int(input())
+aux = 0
 
-for i in 
+if(Y < X):
+    aux = X
+    X = Y
+    Y = aux
+
+for i in range(X, Y):
+    if(X % 5 == 2 or X % 5 == 3):
+        print(i)
+    X = X + 1
