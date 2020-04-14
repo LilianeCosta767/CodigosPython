e4a76430d8d220573ad6aac5939a05e1c51f220d
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-04-14 12:00:59 -0300

    resolvendo a ampulheta

--- ampulheta.py
+++ ampulheta.py
@@ -1,54 +1,47 @@
-#ampulhetas
+#questão ampulheta
+
 largura_amp = 0
 qtd_pontos = 0
 ind_matriz = 0
 cont_atribuicao = 0 #contador para atribuição do vetor da ampulheta
+ind_linha = 0
+ind_coluna = 0
+
 
 N_casos = int(input()) #valores de casos
 if(N_casos >= 0):
-    contAmpulheta = 0
+    cont_ampulheta = 0 #contador de ampulhetas
 
-    while contAmpulheta <= N_casos: #pega os valores de l e i
-        altura_amp = int(input())
-        altura_pontos = int(input())
+    while cont_ampulheta <= N_casos: 
+        print('entrei no while')
+        altura_amp = int(input()) #valor de l
+        altura_pontos = int(input()) #valor de i
         if altura_amp > 3 and altura_amp < 40:
             if altura_pontos < (altura_amp - 2):
-                #criação da matriz
-                matriz_ampulheta = [[] * altura_amp, [] * largura_amp]
-
                 #contagem da largura da ampulheta
-                largura_amp = N_casos+altura_amp+altura_pontos
+                largura_amp = N_casos + altura_amp + altura_pontos
                 if(largura_amp % 2 == 0): #verifica se é par
                     largura_amp = largura_amp + 1 #largura final da ampulheta
                 else:
-                    largura_amp = largura_amp + 2
+                    largura_amp = largura_amp + 2 #largura final da ampulheta
+
+                #criação da matriz
+                print('teste')
+                matriz_ampulheta = [[0] * altura_amp, [0] * largura_amp]
 
                 #contagem dos pontos de areia
                 qtd_pontos = largura_amp - 6
 
-                #atualizando altura da ampulhera
+                #atualizando altura da ampulheta
                 altura_amp = altura_amp * 2
-                ind_matriz = altura_amp
 
                 #atribuição da ampulheta
-                for ind_matriz in range(len(altura_amp)): #percorre as linhas
-                    while(cont_atribuicao <= largura_amp): #percorre as colunas
-                        if(cont_atribuicao == 0 or cont_atribuicao == 1):
-                            matriz_ampulheta[ind_matriz][cont_atribuicao] = '*'
-                        elif(cont_atribuicao == largura_amp or cont_atribuicao == (largura_amp - 1)):
-                            matriz_ampulheta[ind_matriz][cont_atribuicao] = '*'
-                        elif(matriz_ampulheta[ind_matriz-1] == '*'):
-                            matriz_ampulheta[ind_matriz][cont_atribuicao] = ' *'
-
-                        # elif(ind_vet == (altura_amp / 2) or ind_vet == (altura_amp / 2) + 1): #encontra o meio da ampulheta
-                        #     vet_ampulheta[ind_vet] = '?'
-                        cont_atribuicao = cont_atribuicao + 1
-                    ind_vet = ind_vet - 1
-
-                #ind_vet = 0 #zerando o indice
+                
 
                 #impressão da ampulheta
-                for ind_vet in range(len(altura_amp)):
-                    print(matriz_ampulheta)
+                for ind_linha in range(altura_amp):
+                    for ind_coluna in range(largura_amp):
+                        print(matriz_ampulheta[ind_linha][ind_coluna], end = ' ')
+                    print()
 
-                contAmpulheta = contAmpulheta + 1 #vai para proxima ampulheta
+        cont_ampulheta = cont_ampulheta + 1 #vai para proxima ampulheta
--- teste.py
+++ teste.py
@@ -1,25 +1,9 @@
-altura_amp = 1
-ind_vet = 0
-cont_atribuicao = 0 #contador da coluna
-largura_amp = 11
+altura_amp = 5
+largura_amp = 2
 
-matriz = [[0] * altura_amp, [0] * largura_amp]
+matriz_ampulheta = [[] * altura_amp, [] * largura_amp]
 
-print(matriz)
-
-
-# vet_ampulheta = colunas_amp + linhas_amp
-
-
-
-# while(cont_atribuicao <= largura_amp): #percorre as colunas
-#     if(cont_atribuicao == 0 or cont_atribuicao == 1):
-#         vet_ampulheta[ind_vet][cont_atribuicao] = '*'
-#     elif(cont_atribuicao == largura_amp or cont_atribuicao == (largura_amp - 1)):
-#         vet_ampulheta[ind_vet][cont_atribuicao] = '*'
-#     elif(vet_ampulheta[ind_vet-1] == '*'):
-#         vet_ampulheta[ind_vet][cont_atribuicao] = ' *'
-#     cont_atribuicao = cont_atribuicao + 1
-
-# for ind_vet in range(len(altura_amp)):
-#     print(vet_ampulheta)
+for ind_linha in range(altura_amp):
+    for ind_coluna in range(largura_amp):
+        print(matriz_ampulheta[ind_linha][ind_coluna], end = ' ')
+    print()
