9ef3704b722339809a6783ec8f3a9b2e8b97953f
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-04-27 10:38:38 -0300

    exercicios de lista em python

--- teste.py
+++ teste.py
@@ -1,9 +1,5 @@
-import random
+import math
 
-a1 = input('Nome do aluno 1: ')
-a2 = input('Nome do aluno 2: ')
-a3 = input('Nome do aluno 3: ')
-a4 = input('Nome do aluno 4: ')
-lista = [a1, a2, a3, a4]
-seq = random.sample(lista, 4)
-print('A ordem sorteada foi: ', seq)
+raiz = math.sqrt(9)
+
+print(raiz)
