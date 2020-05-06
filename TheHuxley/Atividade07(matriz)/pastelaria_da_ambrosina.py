4c1a058c372370bd9c209deb9ff36e50b47acac6
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-05-06 11:09:49 -0300

    resolucao da atividade pastelaria da ambrosina do thehuxley com python

--- TheHuxley/Atividade07(matriz)/pastelaria_da_ambrosina.py
+++ TheHuxley/Atividade07(matriz)/pastelaria_da_ambrosina.py
@@ -1,18 +1,24 @@
 # declarações
 matriz = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 aux = 0
-#cont_aux = 0
+valor = [0] * 3
+soma = 0
 
 # resolução do problema
-# pegando os valores da matriz
-for l in range(4):
-    for c in range(4):
-        aux = input().split()
-        for i in range(4):
-            matriz[l][c] = float(aux[i])
-    
-# imprimindo a matriz
-for l in range(4):
-    for c in range(4):
-        print(matriz[l][c], end = ' ')
-    print()
+for l in range(4): #percorre as linhas
+    #for i in range(4): #leitura da linha como um input
+    aux = input().split()
+    for c in range(4): #atribuição da linha informada na linha correspondente da matriz
+        matriz[l][c] = float(aux[c])
+
+
+# fazendo a conta dos preços do pastel
+for l in range(3): # pegaremos apenas a linha do produto
+    for c in range(4): # pegamos todas as colunas
+        soma = soma +  (matriz[3][c] * matriz[l][c])
+    valor[l] = soma
+    soma = 0
+
+# imprimindo o resultado
+for i in range(3):
+    print('{:.2f}'.format(valor[i]))
--- teste.py
+++ teste.py
@@ -1,16 +1,3 @@
+valor = [0 * 3]
 
-lista1 = [1,2,4]
-lista2 = [3,10,9,7,6]
-
-cont_lista1 = 0
-cont_lista2 = 0
-
-intercalada = [0] * (len(lista1) + len(lista2))
-
-print(len(intercalada))
-
-for i in range(len(intercalada)):
-    intercalada[i] = lista1[cont_lista1]
-    cont_lista1 = cont_lista1 + 1
-
-print (intercalada)
+print(valor)
