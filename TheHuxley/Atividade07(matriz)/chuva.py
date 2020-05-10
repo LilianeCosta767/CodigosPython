20a76d5f84acd64a841c9138ae0d56975a42a4a0
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-05-10 12:04:24 -0300

    Resolucao da atividade chuva do thehuxley em python

--- TheHuxley/Atividade07(matriz)/chuva.py
+++ TheHuxley/Atividade07(matriz)/chuva.py
@@ -1,15 +1,33 @@
 # declarações
 N = 0 
 matriz = []
+aux = 0
+vet_aux = []
+soma = 0
+soma_aux = []
+soma_matriz = []
 
 N = int(input())
 if(N >= 1 and N <= 100):
+    vet_aux = [0] * N
+    soma_aux = [0] * N
     for l in range(2*N): # qtd de linhas 
         aux = input().split()
         for c in range(N): # sao N colunas pre-determinadas
-            matriz[l][c] = int(aux[c])
+            vet_aux[c] = int(aux[c])
+        matriz.append(vet_aux[:])
 
-for l in range(2*N):
+# somando as matrizes
+for l in range(N): 
     for c in range(N):
-        print(matriz)
+        soma = matriz[l][c] + matriz[l+N][c]
+        soma_aux[c] = soma
+    soma_matriz.append(soma_aux)
+    soma_aux = [0] * N 
+
+
+
+for l in range(N):
+    for c in range(N):
+        print(soma_matriz[l][c], end = ' ')
     print()
